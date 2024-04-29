077eb8d8115e0ab06b33865bc3b2969963a3670a
Pirson Bethancourt <pirsonbethancourt039@gmail.com>
2024-04-29 13:27:36 -0400

    changes

--- shell.nix
+++ shell.nix
@@ -3,7 +3,6 @@ let
 in pkgs.mkShell {
   packages = [
     (pkgs.python3.withPackages (python-pkgs: [
-      # select Python packages here
       python-pkgs.matplotlib
       python-pkgs.mpmath
       python-pkgs.numpy
